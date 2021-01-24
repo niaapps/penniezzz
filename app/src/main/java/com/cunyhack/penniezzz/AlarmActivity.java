09a6b876cfe28a15bb2c2dbdc29345ee0aa46803
Ahsanul <abeerahsanul@gmail.com>
2021-01-24 16:06:23 -0500

    Coded navigation between the activities

--- app/src/main/java/com/cunyhack/penniezzz/AlarmActivity.java
+++ app/src/main/java/com/cunyhack/penniezzz/AlarmActivity.java
@@ -2,6 +2,7 @@ package com.cunyhack.penniezzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class AlarmActivity extends AppCompatActivity {
     TextView congratText;
     Button wakeUp;
     Button payUp;
+    Button configureSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +27,14 @@ public class AlarmActivity extends AppCompatActivity {
         currentTime = findViewById(R.id.currentTime);
 //        Finds the ID of the the wakeUp Button and the payUp Button
         wakeUp = findViewById(R.id.wakeUp);
-        payUp =  findViewById(R.id.payUp);
+//        payUp =  findViewById(R.id.payUp);
+        configureSettings = findViewById(R.id.configureSettingsButton);
+        configureSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSetupActivity();
+            }
+        });
 
         congratText = findViewById(R.id.congratText);
 //        Now we need to listen on the button clicks
@@ -36,8 +45,14 @@ public class AlarmActivity extends AppCompatActivity {
             }
         });
 
+
         final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
 
 
+    }
+
+    private void openSetupActivity() {
+        Intent intent = new Intent(this, SetupActivity.class);
+        startActivity(intent);
     }
 }
