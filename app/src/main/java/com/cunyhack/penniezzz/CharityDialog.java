ac699696fe47330a85e21945f33ceb964a0f5fd6
Arsela Sana Mallick <arselam16@arselas-macbook.myfiosgateway.com>
2021-02-03 19:35:57 -0500

    charity causes and orgs

--- app/src/main/java/com/cunyhack/penniezzz/CharityDialog.java
+++ app/src/main/java/com/cunyhack/penniezzz/CharityDialog.java
@@ -18,36 +18,75 @@ public class CharityDialog extends AppCompatDialogFragment {
     ListView lvCause, lvO;
     String [] causeList;
     String[] wList;
+    String[] hList;
+    String[] jList;
+    String[] eduList;
+    String[] cList;
+    String[] envList;
+
     AlertDialog.Builder secondBuilder;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         causeList = getActivity().getResources().getStringArray(R.array.causesOptions);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.cause_dialog, null);
-        ArrayAdapter<String> adapterC= new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,causeList);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()); //alert dialog builder
+        LayoutInflater inflater = getActivity().getLayoutInflater(); //this is the pop up on the screen
+        View view = inflater.inflate(R.layout.cause_dialog, null); //creates view for the user
+        ArrayAdapter<String> adapterC= new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,causeList); //the layout of listview
         builder.setTitle("Choose a Cause");
         lvCause = view.findViewById(R.id.lvCause);
         lvCause.setAdapter(adapterC);
-        builder.setView(view);
+        builder.setView(view); //shows view to builder
         AlertDialog alert = builder.create();
-        lvCause.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        lvCause.setOnItemClickListener(new AdapterView.OnItemClickListener() { //OnClickListener tells button what to do
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) { //Item listener
 
                 String value = (String) parent.getItemAtPosition(position);
 
-                if(value.equals("Women")){
+                if(value.equals("Women's Rights")){
                     wList = getActivity().getResources().getStringArray(R.array.womensOptions);
 
                     showSecondDialog(wList);
                     alert.dismiss();
-
-
-
                 }
+
+                if(value.equals("Health")){
+                    hList = getActivity().getResources().getStringArray(R.array.healthOptions);
+
+                    showSecondDialog(hList);
+                    alert.dismiss();
+                }
+
+                if(value.equals("Justice")){
+                    jList = getActivity().getResources().getStringArray(R.array.justiceOptions);
+
+                    showSecondDialog(jList);
+                    alert.dismiss();
+                }
+
+                if(value.equals("Education")){
+                    eduList = getActivity().getResources().getStringArray(R.array.eduOptions);
+
+                    showSecondDialog(eduList);
+                    alert.dismiss();
+                }
+
+                if(value.equals("Covid-19")){
+                    cList = getActivity().getResources().getStringArray(R.array.covidOptions);
+
+                    showSecondDialog(cList);
+                    alert.dismiss();
+                }
+
+                if(value.equals("Environment")){
+                    envList = getActivity().getResources().getStringArray(R.array.envOptions);
+
+                    showSecondDialog(envList);
+                    alert.dismiss();
+                }
+
             }
         });
 
